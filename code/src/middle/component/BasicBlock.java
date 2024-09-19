a387e547a6ce0de1086be0a0bbb9f2084595a167
Jew <Zhu149248@buaa.edu.cn>
2024-09-19 21:51:44 +0800

    sb

--- code/src/middle/component/BasicBlock.java
+++ code/src/middle/component/BasicBlock.java
@@ -7,6 +7,7 @@ import middle.component.types.LabelType;
 import tools.Builder;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class BasicBlock extends Value {
     private final ArrayList<Instruction> instructions = new ArrayList<>();
@@ -14,6 +15,11 @@ public class BasicBlock extends Value {
     private final ArrayList<BasicBlock> prevBlocks = new ArrayList<>();
     // 后继基本块的集合
     private final ArrayList<BasicBlock> nextBlocks = new ArrayList<>();
+    // 支配的基本块的集合
+    private HashSet<BasicBlock> dominantees = null;
+    // 被直接支配的基本块
+    private BasicBlock immediateDominator = null;
+    private HashSet<BasicBlock> immediateDominants = new HashSet<>();
     private boolean isTerminated = false;
 
     public BasicBlock(Function function) {
@@ -57,6 +63,33 @@ public class BasicBlock extends Value {
 
     public void addNextBlock(BasicBlock nextBlock) {
         nextBlocks.add(nextBlock);
+    }
+
+    public void setDominantees(HashSet<BasicBlock> dominantees) {
+        this.dominantees = dominantees;
+    }
+
+    public HashSet<BasicBlock> getDominantees() {
+        return dominantees;
+    }
+
+    public boolean dominant(BasicBlock block) {
+        if (dominantees == null) {
+            throw new IllegalStateException("dominantees not set");
+        }
+        return dominantees.contains(block);
+    }
+
+    public boolean strictDominant(BasicBlock block) {
+        if (dominantees == null) {
+            throw new IllegalStateException("dominantees not set");
+        }
+        return dominantees.contains(block) && !block.equals(this);
+    }
+
+    public void setImmediateDominator(BasicBlock immediateDominator) {
+        this.immediateDominator = immediateDominator;
+        immediateDominator.immediateDominants.add(this);
     }
 
     // 当有新的变量引入时，需要跳转到之后还未构建的基本块，进行重填
--- code/src/optimize/Mem2Reg.java
+++ code/src/optimize/Mem2Reg.java
@@ -22,6 +22,57 @@ public class Mem2Reg {
         BasicBlock firstBlock = function.getBasicBlocks().get(0);
         for (BasicBlock targetBlock : function.getBasicBlocks()) {
             HashSet<BasicBlock> visited = new HashSet<>();
+            visit(firstBlock, targetBlock, visited);
+            HashSet<BasicBlock> dominatee = new HashSet<>();
+            for (BasicBlock block : function.getBasicBlocks()) {
+                if (!visited.contains(block)) {
+                    dominatee.add(block);
+                }
+            }
+            targetBlock.setDominantees(dominatee);
         }
+    }
+
+    private static void visit(BasicBlock block, BasicBlock target, HashSet<BasicBlock> visited) {
+        if (target.equals(block)) {
+            return;
+        }
+        visited.add(block);
+        for (BasicBlock basicBlock : block.getNextBlocks()) {
+            if (!visited.contains(basicBlock)) {
+                visit(basicBlock, target, visited);
+            }
+        }
+    }
+
+    private static void runImmediateDominator(Function function) {
+        for (BasicBlock basicBlock : function.getBasicBlocks()) {
+            getImmediateDominator(basicBlock);
+        }
+    }
+
+    private static void getImmediateDominator(BasicBlock basicBlock) {
+        // 计算由basicBlock支配的基本块中，哪些属于直接支配
+        for (BasicBlock block : basicBlock.getDominantees()) {
+            // 最近的严格支配，不能是自己
+            if (block.equals(basicBlock)) {
+                continue;
+            }
+            boolean flag = true;
+            for (BasicBlock block1 : basicBlock.getDominantees()) {
+                if (block1.equals(basicBlock)) {
+                    continue;
+                }
+                if (block1.dominant(block)) {
+                    // 此时block1与basicBlock相比距离block更近
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                block.setImmediateDominator(basicBlock);
+            }
+        }
+
     }
 }
