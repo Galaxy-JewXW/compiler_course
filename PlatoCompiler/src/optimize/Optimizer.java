c940e63e5f2e5fa9b730bb8e101576904c26a804
Jew <22373498@buaa.edu.cn>
2024-09-24 22:30:05 +0800

    添加常数条件表达式计算

--- PlatoCompiler/src/optimize/Optimizer.java
+++ PlatoCompiler/src/optimize/Optimizer.java
@@ -10,6 +10,8 @@ public class Optimizer {
         DeadCode.run(module);
         GVN.run(module);
         DeadCode.run(module);
+        CalcCond.run(module);
+        DeadCode.run(module);
         // 序号重命名必须是最后一条
         module.updateId();
     }
