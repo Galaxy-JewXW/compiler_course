82abf998317b6e0b254e822eda2da7cb3ffee21a
Jew <Zhu149248@buaa.edu.cn>
2024-09-26 15:35:51 +0800

    man

--- PlatoCompiler/src/optimize/Optimizer.java
+++ PlatoCompiler/src/optimize/Optimizer.java
@@ -13,19 +13,16 @@ public class Optimizer {
         DeadCode.run(module);
         InlinedFunction.run(module);
         DeadCode.run(module);
-        UnusedBasicBlock.run(module);
-        UnusedFunction.run(module);
         module.updateId();
         Printer.printIr(module, "ir_phi.txt");
         GVN.run(module);
         DeadCode.run(module);
-        DeadCode.run(module);
         CertainBranch.run(module);
         OptimizePhi.run(module);
         InlinedFunction.run(module);
-        DeadCode.run(module);
         UnusedBasicBlock.run(module);
         UnusedFunction.run(module);
+        DeadCode.run(module);
         // 序号重命名必须是最后一条
         module.updateId();
     }
